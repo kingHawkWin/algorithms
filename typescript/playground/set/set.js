0ea82f67a10b6c2ca87dedc0cefdc09671fccd8e
kingHawkWin <kinghawkwin@icloud.com>
2020-12-26 22:30:05 +0800

    feat(ts): set
    ts set

--- typescript/playground/set/set.js
+++ typescript/playground/set/set.js
@@ -1,2 +1,31 @@
 "use strict";
-const set = new Set();
+/*
+[
+  'has',
+  'add',
+  'delete',
+  'clear',
+  'entries',
+  'forEach',
+  'size',
+  'values',
+  'keys',
+]
+ */
+const set = new Set(); // create
+set.add(1);
+console.log(set);
+console.log(set.has(1));
+set.delete(1);
+console.log(set);
+set.add(1);
+set.add(2);
+console.log(set);
+console.log(set.size);
+// traversal
+console.log(set.keys());
+console.log(set.values());
+console.log(set.entries());
+set.forEach((v, k, s) => {
+    console.log(`${v} - ${k} - ${s}`);
+});
--- typescript/playground/set/set.ts
+++ typescript/playground/set/set.ts
@@ -1,1 +1,33 @@
-const set: Set<number> = new Set()
+/*
+[
+  'has',
+  'add',
+  'delete',
+  'clear',
+  'entries',
+  'forEach',
+  'size',
+  'values',
+  'keys',
+]
+ */
+
+const set: Set<number> = new Set() // create
+
+set.add(1)
+console.log(set)
+console.log(set.has(1))
+set.delete(1)
+console.log(set)
+set.add(1)
+set.add(2)
+console.log(set)
+console.log(set.size)
+
+// traversal
+console.log(set.keys())
+console.log(set.values())
+console.log(set.entries())
+set.forEach((v, k, s) => {
+  console.log(`${v} - ${k} - ${s}`)
+})
